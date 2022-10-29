a0c062a199a217261cce7c47a26ffbafa1669ba8
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-10-29 14:35:13 +0800

    minor fix

--- .dev_scripts/train_benchmark.sh
+++ .dev_scripts/train_benchmark.sh
@@ -2,5 +2,5 @@ PARTITION=$1
 WORK_DIR=$2
 CPUS_PER_TASK=${3:-4}
 
-echo 'configs/detection/fft_filter_experiment1/baseline_faster.py' &
+echo 'configs/detection/detector_with_enhance_head/faster-rcnn_r50_fpn_basic-enhance_1x_coco.py' &
 GPUS=8  GPUS_PER_NODE=8  CPUS_PER_TASK=$CPUS_PRE_TASK ./tools/slurm_train.sh $PARTITION example-1 configs/detection/detector_with_enhance_head/faster-rcnn_r50_fpn_basic-enhance_1x_coco.py $WORK_DIR/detector_with_enhance_head/faster_r50_1x_basic_enhance_head --cfg-options default_hooks.checkpoint.max_keep_ckpts=1 &
