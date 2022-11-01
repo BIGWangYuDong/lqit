9144087a448c4c4dfd055e827cedb2b7accc597d
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-11-01 19:50:58 +0800

    [Enhance] Update EDFFNet cfg

--- configs/detection/edffnet/edffnet.py
+++ configs/detection/edffnet/edffnet.py
@@ -2,13 +2,13 @@ _base_ = '../edffnet/atss_r50_fpn_1x.py'
 
 model = dict(
     type='EDFFNet',
-    backbone=dict(norm_eval=False),
+    backbone=dict(norm_eval=True),
     neck=dict(
         type='DFFPN',
         in_channels=[256, 512, 1024, 2048],
         out_channels=256,
         start_level=1,
-        add_extra_convs='on_output',
+        add_extra_convs='on_input',
         shape_level=2,
         num_outs=5),
     enhance_head=dict(
@@ -16,24 +16,40 @@ model = dict(
         in_channels=256,
         feat_channels=256,
         num_convs=5,
-        loss_enhance=dict(type='lqit.L1Loss', loss_weight=0.7),
+        loss_enhance=dict(type='mmdet.L1Loss', loss_weight=0.7),
         gt_preprocessor=dict(
             type='lqit.GTPixelPreprocessor',
-            mean=[123.675],
-            std=[58.395],
+            mean=[128],
+            std=[57.12],
             pad_size_divisor=32,
-            element_name='edge')),
-)
+            element_name='edge')))
 
 # dataset settings
 train_pipeline = [
-    dict(
-        type='LoadImageFromFile',
-        file_client_args={{_base_.file_client_args}}),
+    dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', prob=0.5),
     dict(type='lqit.GetEdgeGTFromImage', method='scharr'),
-    dict(type='lqit.PackInputs')
+    dict(
+        type='lqit.TransBroadcaster',
+        src_key='img',
+        dst_key='gt_edge',
+        transforms=[
+            dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+            dict(type='RandomFlip', prob=0.5)
+        ]),
+    dict(type='lqit.PackInputs', )
 ]
 train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+
+param_scheduler = [
+    dict(
+        type='LinearLR', start_factor=0.001, by_epoch=False, begin=0,
+        end=1000),
+    dict(
+        type='MultiStepLR',
+        begin=0,
+        end=12,
+        by_epoch=True,
+        milestones=[8, 11],
+        gamma=0.1)
+]
