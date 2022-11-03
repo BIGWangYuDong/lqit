0d31cb59e4e54eb1bd7a1582062e1c550110dcf1
BigDong <yudongwang@tju.edu.cn>
2022-11-03 17:18:29 +0800

    [Feature] Support XMLDatasetWithMetaFile (#11)
    * [Feature] Support XMLDatasetWithMetaFile
    
    * update

--- lqit/detection/datasets/__init__.py
+++ lqit/detection/datasets/__init__.py
@@ -1,4 +1,9 @@
+from .class_names import *  # noqa: F401,F403
 from .rtts import RTTSCocoDataset
 from .urpc import URPCCocoDataset, URPCXMLDataset
+from .xml_dataset import XMLDatasetWithMetaFile
 
-__all__ = ['URPCCocoDataset', 'URPCXMLDataset', 'RTTSCocoDataset']
+__all__ = [
+    'XMLDatasetWithMetaFile', 'URPCCocoDataset', 'URPCXMLDataset',
+    'RTTSCocoDataset'
+]
