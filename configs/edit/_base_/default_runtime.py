37e003dbd2383137bab7f39fbb6594704a13dba8
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-11-06 15:17:03 +0800

    [Feature] Support BasicImageDataset

---
+++ configs/edit/_base_/default_runtime.py
@@ -0,0 +1,27 @@
+default_scope = 'lqit'
+
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(type='CheckpointHook', interval=1, max_keep_ckpts=1),
+    sampler_seed=dict(type='DistSamplerSeedHook'))
+# visualization=dict(type='DetVisualizationHook')  TODO: not implement yet
+
+# set seed = 0 as default
+randomness = dict(seed=0, deterministic=False)
+
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'),
+)
+
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='Visualizer', vis_backends=vis_backends, name='visualizer')
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+
+log_level = 'INFO'
+load_from = None
+resume = False
