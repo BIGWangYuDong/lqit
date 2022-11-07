1e3c3ed2771a4394c948037c072d06bf08ea298f
hewanru-bit <2558905977@qq.com>
2022-11-07 17:03:26 +0800

    up

---
+++ configs/edit/_base_/datasets/cityscape_enhancement_with_anno.py
@@ -0,0 +1,75 @@
+# dataset settings
+dataset_type = 'mmdet.CityscapesDataset'
+data_root = '/home/tju531/hwr/Datasets/'
+
+file_client_args = dict(backend='disk')
+
+train_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadGTImageFromFile', file_client_args=file_client_args),
+    dict(
+        type='TransBroadcaster',
+        src_key='img',
+        dst_key='gt_img',
+        transforms=[
+            dict(type='Resize', scale=(512, 512), keep_ratio=True),
+            dict(type='RandomFlip', prob=0.5),
+        ]),
+    dict(type='PackInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', file_client_args=file_client_args),
+    dict(type='LoadGTImageFromFile', file_client_args=file_client_args),
+    dict(
+        type='TransBroadcaster',
+        src_key='img',
+        dst_key='gt_img',
+        transforms=[dict(type='Resize', scale=(512, 512), keep_ratio=True)]),
+    dict(
+        type='PackInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type='lqit.DatasetWithClearImageWrapper',
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file='cityscape_foggy/annotations_json/instancesonly_filtered_gtFine_train.json',
+            data_prefix=dict(img='cityscape_foggy/train/', gt_img_path='cityscape/train/'),
+            filter_cfg=dict(filter_empty_gt=True, min_size=32),
+            pipeline=train_pipeline),
+    suffix='png'
+    ))
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='lqit.DatasetWithClearImageWrapper',
+        dataset=dict(
+            type=dataset_type,
+            data_root=data_root,
+            test_mode=True,
+            indices=100,
+            ann_file='cityscape_foggy/annotations_json/instancesonly_filtered_gtFine_test.json',
+            data_prefix=dict(img='cityscape_foggy/test/', gt_img_path='cityscape/test/'),
+            filter_cfg=dict(filter_empty_gt=True, min_size=32),
+            pipeline=test_pipeline),
+        suffix='png'
+    ))
+
+test_dataloader = val_dataloader
+
+val_evaluator = [
+    dict(type='MSE', gt_key='img', pred_key='pred_img'),
+]
+test_evaluator = val_evaluator
--- configs/edit/aodnet/aodnet.py
+++ configs/edit/aodnet/aodnet.py
@@ -1,7 +1,8 @@
 _base_ = [
-    '../../detection/_base_/schedules/schedule_1x.py', '../../detection/_base_/default_runtime.py'
+    '../_base_/datasets/cityscape_enhancement_with_txt.py',
+    '../_base_/schedules/schedule_1x.py',
+    '../_base_/default_runtime.py'
 ]
-
 model = dict(
     type='lqit.BaseEditModel',
     data_preprocessor=dict(
@@ -18,45 +19,6 @@ model = dict(
         pixel_loss=dict(type='MSELoss', loss_weight=1.0)
        ))
 
-# dataset settings
-dataset_type = 'CityscapesDataset'
-data_root = '/home/tju531/hwr/Datasets/'
-
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='lqit.LoadGTImageFromFile'),
-    dict(
-        type='lqit.TransBroadcaster',
-        src_key='img',
-        dst_key='gt_img',
-        transforms=[
-            dict(type='Resize', scale=(256, 256), keep_ratio=True),
-            dict(type='RandomFlip', prob=0.5)
-        ]),
-    dict(type='lqit.PackInputs', )
-]
-
-train_dataloader = dict(
-    batch_size=2,
-    num_workers=2,
-    persistent_workers=True,
-    sampler=dict(type='DefaultSampler', shuffle=True),
-    batch_sampler=dict(type='AspectRatioBatchSampler'),
-    dataset=dict(
-        type='lqit.DatasetWithClearImageWrapper',
-        dataset=dict(
-            type=dataset_type,
-            data_root=data_root,
-            ann_file='cityscape_foggy/annotations_json/instancesonly_filtered_gtFine_train.json',
-            data_prefix=dict(img='cityscape_foggy/train/', gt_img_path='cityscape/train/'),
-            filter_cfg=dict(filter_empty_gt=True, min_size=32),
-            pipeline=train_pipeline),
-    suffix='png'
-    ))
-
-val_dataloader = None
-val_cfg = None
-test_cfg = None
 
 train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=10, val_interval=1)
 param_scheduler = [
