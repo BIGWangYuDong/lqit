f9b1d6f81479afd7e37e243b403844917cb196be
BIGWangYuDong <yudongwang@tju.edu.cn>
2022-12-01 11:00:07 +0800

    [Feature] Support raw urpc detectors configs

--- configs/detection/urpc_detection/faster_rcnn_r50_fpn_1x_urpc_xml.py
+++
@@ -1,7 +0,0 @@
-_base_ = [
-    '../_base_/models/faster-rcnn_r50_fpn.py',
-    '../_base_/datasets/urpc-2020_xml_detection.py',
-    '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
-]
-
-model = dict(roi_head=dict(bbox_head=dict(num_classes=4)))
