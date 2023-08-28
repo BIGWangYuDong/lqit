290313a3d8a5bb3b9d365023a31addef0622d900
BIGWangYuDong <wangyudong@pjlab.org.cn>
2023-08-29 01:55:55 +0800

    fix new config error

---
+++ configs/detection/urpc2020_dataset/train_validation/faster-rcnn_r50_fpn_1x_urpc-xml.py
@@ -0,0 +1,7 @@
+_base_ = [
+    '../../_base_/models/faster-rcnn_r50_fpn.py',
+    '../../_base_/datasets/urpc2020/urpc2020-validation_xml_detection.py',
+    '../../_base_/schedules/schedule_1x.py', '../../_base_/default_runtime.py'
+]
+
+model = dict(roi_head=dict(bbox_head=dict(num_classes=4)))
